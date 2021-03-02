a9ee5d5d564bd51ddf9161ea47a2517d18741b99
Minibumphead <minibumphead281@gmail.com>
2021-03-02 10:56:09 +0100

    removed console.log

--- src/services/index.js
+++ src/services/index.js
@@ -14,9 +14,7 @@ export const login = async(formData) => {
 
         try {
             const response = await axios.post('http://localhost:5000/users/login', formData)
-            console.log(response.data)
             localStorage.setItem("user", JSON.stringify(response.data))
-           
             return response.data
         } catch(error) {
             console.log(error)
